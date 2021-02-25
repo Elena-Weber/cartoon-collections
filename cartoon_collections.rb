0ba9f628910e8710482f441708c375cd0879602d
Elena-Weber <crochetmiracles@gmail.com>
2021-02-25 16:24:33 -0500

    commit tree dcb32dc3c75302c300314a8d782532e984e95300

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,24 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarves)# code an argument here
+  dwarves.collect.with_index(1) do |name, i|
+    puts "#{i}. #{name}" # Your code here
+  end
+end
+
+def summon_captain_planet(planeteer_calls) # code an argument here
+  planeteer_calls.map do |name|
+    name.capitalize + "!"
+  end
   # Your code here
 end
 
-def summon_captain_planet# code an argument here
+def long_planeteer_calls(calls) # code an argument here
+  calls.any?{|word| word.length > 4}
   # Your code here
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
-
-def find_the_cheese# code an argument here
+def find_the_cheese(array) # code an argument here
+  
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find {|cheese| cheese_types.include?(cheese)}
 end
